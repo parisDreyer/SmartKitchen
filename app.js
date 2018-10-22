444eda5cc656c364e48d26663cfeb13f815e622c
Chris Yip <chris.yip87@gmail.com>
2018-10-22 11:03:45 -0700

    Setup db and user references for app.js

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+
 const port = process.env.PORT || 5000;
+
+// app.use("/api/users", users);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+  
+
+
+
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
