efe85a9c40fb757629c7482f095e827df6ebb4a5
Chris Yip <chris.yip87@gmail.com>
2018-10-22 11:33:31 -0700

    Add api/users/login route with working login. Registration also logs in when successful

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
+const jsonwebtoken = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 router.post('/register', (req, res) => {
     User.findOne({ email: req.body.email })
@@ -19,7 +21,22 @@ router.post('/register', (req, res) => {
                         if(err) throw err;
                         newUser.password = hash;
                         newUser.save()
-                            .then(user => res.json(user))
+                            .then(user => {
+                                const payload = { id: user.id, email: user.email };
+
+                                jsonwebtoken.sign(
+                                    payload,
+                                    keys.secretOrKey,
+                                    //expires in 24 hours
+                                    { expiresIn: 86400 },
+                                    (err, token) => {
+                                        res.json({
+                                            success: true,
+                                            token: 'Bearer ' + token
+                                        });
+                                    }
+                                );
+                            })
                             .catch(err => console.log(err));
                     })
                 })
@@ -27,9 +44,38 @@ router.post('/register', (req, res) => {
         })
 });
 
-// router.post('/login', (req, res) => {
-//     const email = req.body.email;
-//     const password = req.body.password;
-// })
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: 'This user does not exist' });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        const payload = {id: user.id, email: user.email};
+
+                        jsonwebtoken.sign(
+                            payload,
+                            keys.secretOrKey,
+                            //expires in 24 hours
+                            { expiresIn: 86400 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            }
+                        );
+                    } else {
+                        return res.status(400).json({ password: 'Incorrect password' });
+                    }
+                })
+        })
+});
 
 module.exports = router;
