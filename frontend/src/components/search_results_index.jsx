fda3e52bb2f2b081b088006594388056c3679305
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-22 10:42:24 -0700

    Create skeleton for search results index component and search results index container component

---
+++ frontend/src/components/search_results_index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchResultsIndex extends React.Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+
+    }
+}
+
+export default SearchResultsIndex;
---
+++ frontend/src/components/search_results_index_container.js
@@ -0,0 +1,15 @@
+import { connect } from "react-redux";
+import SearchResultsIndex from './search_results_index';
+
+const mapStateToProps = state => {
+
+};
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SearchResultsIndex);
