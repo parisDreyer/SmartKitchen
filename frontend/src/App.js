368832dedef535b00795c898286900daee7c2776
Luke Dreyer <lukepdreyer@gmail.com>
2018-10-23 08:39:28 -0700

    wrapped store in provider so we can pass store to components from app.js   -- We can refactor some of app.js to a root component in the components directory. Made all the code hold together without compiler errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider} from 'react-redux';
 import Dictaphone from './components/dictaphone.jsx';
 import SearchIndexContainer from './components/recipes/search_results_index_container';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-            Learn React
-        </header>
-        hi
-        <Dictaphone />
-        <SearchIndexContainer />
-      </div>
-    );
-  }
+import ConfigureStore from "./store/store";
+const store = ConfigureStore();
+
+
+// need to get this working from the example at
+// https://github.com/znrm/typedraw/blob/master/src/App.js
+// for frontend auth 
+//--------------------------
+// const checkForCurrentUser = async () => {
+//   const token = await getAuthToken();
+//   if (token) {
+//     fetchCurrentSession({ Authorization: token })(store.dispatch);
+//   }
+// };
+
+
+
+const App = () => {
+  return (<div className="App">
+        <header className="App-header">Learn React</header>
+        
+          hi
+          <Dictaphone />
+          <SearchIndexContainer />
+        
+  </div>);
 }
 
-export default App;
+
+const AppContainer = () => {
+  return (<Provider store={store}>
+    <App store={store} />
+  </Provider>);
+}
+
+export default AppContainer;
 
 {/* <img src={logo} className="App-logo" alt="logo" />
 <p>
--- frontend/src/util/recipes_util.js
+++ frontend/src/util/recipes_util.js
@@ -1,6 +1,10 @@
-export const fetchRecipes = (ingredient) => (
-    $.ajax({
-        method: 'GET',
-        url: `/api.edamam.com/search?q=${ingredient}&app_id=6d9d0107&app_key= 4e66ac21b03248caf02cc471ba4749c1`
-    })
-);
+import axios from 'axios';
+
+// export const fetchRecipes = (ingredient) => (
+//     $.ajax({
+//         method: 'GET',
+//         url: `/api.edamam.com/search?q=${ingredient}&app_id=6d9d0107&app_key= 4e66ac21b03248caf02cc471ba4749c1`
+//     })
+// );
+
+export const fetchRecipes = ingredient => axios.get(`/api.edamam.com/search?q=${ingredient}&app_id=6d9d0107&app_key= 4e66ac21b03248caf02cc471ba4749c1`);
