46960b192c32b7b3e16aaf452dfb56d799a6083d
nathasha <nathashasrn@gmail.com>
2018-10-22 20:35:59 -0700

    API ajax actions

---
+++ frontend/actions/recipes_actions.js
@@ -0,0 +1,18 @@
+import * as APIUtil from '../util/recipes_util';
+export const FETCH_RECIPES = 'FETCH_RECIPES';
+;
+
+
+
+
+export const fetchRecipes = (ingredient) => dispatch => (
+    APIUtil.fetchRecipes(ingredient).then(recipes => (dispatch(receiveRecipes(recipes)))
+
+    ));
+
+
+
+export const receiveRecipes = (recipes) => ({
+    type: FETCH_RECIPES,
+    recipes: recipes.hits
+});
---
+++ frontend/reducer/entities_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import recipes from './recipes_reducer';
+
+
+export default combineReducers({
+    recipes
+});
+
