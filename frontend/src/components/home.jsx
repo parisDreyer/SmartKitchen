1f0884e13bd651173ff309bb0c8b27592e6beaa1
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-24 11:55:12 -0700

    npm install

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -6,8 +6,11 @@ export default () => (
     <div className="home-main">
       <h2>Smart Kitchen: A Cooking App for Smart People</h2>
       <h3>What's in your kitchen?</h3>
-      <p>No need to go shopping! Let Smart Kitchen know what you've got on hand, and we'll show you delicious recipes you can make.</p>
-      <Link to="/search" >Get started now!</Link>
+      <p>
+        No need to go shopping! Let Smart Kitchen know what you've got on hand,
+        and we'll show you delicious recipes you can make.
+      </p>
+      <Link to="/search">Get started now!</Link>
     </div>
   </div>
 );
