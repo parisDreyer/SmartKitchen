f98c98d3bb60c239aff1831f6a4f1a3c96306311
Luke Dreyer <lukepdreyer@gmail.com>
2018-10-25 08:17:34 -0700

    add speech_recog css script to frontend src index js -- this is to style the Speech Recognition options

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
+import './css/speech_recog.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
