db12283cf740578fed5f50fe4c03df4f6a3c0bff
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-24 23:16:03 -0700

    Add columns and basic styling to search results index page

--- frontend/src/components/recipes/search_results_index.jsx
+++ frontend/src/components/recipes/search_results_index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from "react-router-dom";
 
 import SearchResultsIndexItem from './search_results_index_item';
+import '../../css/search_results_index.css';
 
 class SearchResultsIndex extends React.Component {
     constructor(props){
@@ -25,8 +26,8 @@ class SearchResultsIndex extends React.Component {
       // const { recipes } = this.props;
       return (
         <section>
-          <h1>Choose a yummy recipe and get cooking!</h1>
-          <ul>
+          <h1 className="yummy">Choose a yummy recipe and get cooking!</h1>
+          <ul className="recipe-list">
             {this.state.recipes.map((recipe, idx) => <SearchResultsIndexItem key={idx} recipe={recipe.recipe} />)}
           </ul>
         </section>
