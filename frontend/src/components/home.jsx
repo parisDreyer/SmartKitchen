9781d840bbea5efb7140871d606e1443196dcb85
nathasha <nathashasrn@gmail.com>
2018-10-25 08:57:12 -0700

    slashpagestyle

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/home.css";
 
 export default () => (
   <div className="home-container">
     <div className="home-main">
-      <h2>Smart Kitchen: A Cooking App for Smart People</h2>
-      <h3>What's in your kitchen?</h3>
-      <p>
-        No need to go shopping! Let Smart Kitchen know what you've got on hand,
+      <h2 className="h2text"> Smart Kitchen: A Cooking App for Smart People</h2>
+      <marquee behavior="alternate" className="slide">
+        What's in your kitchen?
+      </marquee>
+      <p className="noneed">
+        No need to go shopping!
+        <br /> Let Smart Kitchen know what you've got on hand,
+        <br />
         and we'll show you delicious recipes you can make.
       </p>
-      <Link to="/search">Get started now!</Link>
+      <Link to="/search" className="start">Get started now!</Link>
     </div>
   </div>
 );
