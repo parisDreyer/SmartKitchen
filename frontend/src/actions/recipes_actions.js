91f0a94e482af1df964e3e59339ddf7beeea2644
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-26 14:10:46 -0700

    Create action to save ingredients from user input

--- frontend/src/actions/recipes_actions.js
+++ frontend/src/actions/recipes_actions.js
@@ -1,17 +1,17 @@
 import * as APIUtil from '../util/recipes_util';
 export const FETCH_RECIPES = 'FETCH_RECIPES';
-;
+export const SAVE_INGREDIENTS = "SAVE_INGREDIENTS";
 
-
-
-
-export const fetchRecipes = (ingredient) => dispatch => (
-  APIUtil.fetchRecipes(ingredient).then(recipes => (dispatch(receiveRecipes(recipes)))
+export const fetchRecipes = (ingredients) => dispatch => (
+  APIUtil.fetchRecipes(ingredients).then(recipes => (dispatch(receiveRecipes(recipes)))
 ));
 
-
-
 export const receiveRecipes = (recipes) => ({
-    type: FETCH_RECIPES,
-    recipes: JSON.parse(recipes.request.response).hits
+  type: FETCH_RECIPES,
+  recipes: JSON.parse(recipes.request.response).hits
+});
+
+export const saveIngredients = (ingredients) => ({
+  type: SAVE_INGREDIENTS,
+  ingredients
 });
