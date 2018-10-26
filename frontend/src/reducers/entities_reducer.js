c49a05492d69f452d2eb2be60d56b84bc4149ef9
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-26 14:27:04 -0700

    Add saveIngredients dispatch to search form

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -2,9 +2,7 @@ import { combineReducers } from 'redux';
 import recipes from './recipes_reducer';
 import ingredients from './ingredients_reducer';
 
-
 export default combineReducers({
     recipes,
     ingredients
 });
-
