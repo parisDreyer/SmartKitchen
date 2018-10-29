706e009458b4ccb449b5a3fd75bfcc3f4486ee90
Luke Dreyer <lukepdreyer@gmail.com>
2018-10-28 18:11:40 -0700

    render backupRecipe show page

--- frontend/src/components/recipes/backup_recipe_show.jsx
+++ frontend/src/components/recipes/backup_recipe_show.jsx
@@ -6,14 +6,13 @@ class BackupRecipeShow extends React.Component {
     render(){
         
         return (
-            <li className="search-results-index-item">
-                <a href={`recipes/${this.props.match.params.recipeId}`} style={{ display: "table-cell" }} target="_blank">
-                    <span className="recipe-label">{this.props.recipe ? this.props.recipe.label : null}</span>
-                </a>
-                <ul>{this.props.recipe && this.props.recipe.ingredients ? this.props.recipe.ingredients.map(el =>el) : null}</ul>
-
-                <ul>{this.props.recipe && this.props.recipe.ingredients ? this.props.recipe.directions.map(el => el) : null}</ul>
-            </li>
+        <div className="search-results-index-item">
+            <h1 className="recipe-show">{this.props.recipe ? this.props.recipe.label : null}</h1>
+            <h2 className="recipe-show">Ingredients</h2>
+            <ul className="recipe-show">{this.props.recipe && this.props.recipe.ingredients ? this.props.recipe.ingredients.map(el =>el) : null}</ul>
+            <h2 className="recipe-show">Instructions</h2>
+            <ul className="recipe-show">{this.props.recipe && this.props.recipe.ingredients ? this.props.recipe.directions.map(el => el) : null}</ul>
+        </div>
         );
     }
 
