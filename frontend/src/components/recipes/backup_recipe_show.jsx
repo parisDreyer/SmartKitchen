c3e08093fb608114af2ae7c63b3d00733490c38c
Luke Dreyer <lukepdreyer@gmail.com>
2018-10-28 19:33:11 -0700

    add sessionStorage persistance of search results so that results still show after page refresh

--- frontend/src/components/recipes/backup_recipe_show.jsx
+++ frontend/src/components/recipes/backup_recipe_show.jsx
@@ -3,15 +3,22 @@ import React from 'react';
 
 class BackupRecipeShow extends React.Component {
 
+    constructor(props){
+        super(props);
+        if (this.props.recipe) sessionStorage["recipe_show"] = JSON.stringify(this.props.recipe);
+    }
+
+    rec(){
+        return this.props.recipe && this.props.recipe.ingredients ? this.props.recipe : JSON.parse(sessionStorage["recipe_show"]);
+    }
     render(){
-        
         return (
         <div className="search-results-index-item">
-            <h1 className="recipe-show">{this.props.recipe ? this.props.recipe.label : null}</h1>
+            <h1 className="recipe-show">{this.rec() ? this.rec().label : null}</h1>
             <h2 className="recipe-show">Ingredients</h2>
-            <ul className="recipe-show">{this.props.recipe && this.props.recipe.ingredients ? this.props.recipe.ingredients.map(el =>el) : null}</ul>
+            <ul className="recipe-show">{this.rec() && this.rec().ingredients ? this.rec().ingredients.map(el => el) : null}</ul>
             <h2 className="recipe-show">Instructions</h2>
-            <ul className="recipe-show">{this.props.recipe && this.props.recipe.ingredients ? this.props.recipe.directions.map(el => el) : null}</ul>
+            <ul className="recipe-show">{this.rec() && this.rec().ingredients ? this.rec().directions.map(el => el) : null}</ul>
         </div>
         );
     }
