c597aaa53ac5ffa65970e4d74b87a9d6868d6b69
Amanda Mitchell <amandamitchell707@gmail.com>
2018-11-12 15:41:38 -0800

    Add edamam logo to footer and change font to sans serif

--- frontend/src/components/footer/footer_component.jsx
+++ frontend/src/components/footer/footer_component.jsx
@@ -3,39 +3,35 @@ import "../../css/footer.css";
 
 
 class Footer extends React.Component {
-    render() {
-        return <div className="bottom-part">
-            
-              <div className="text">Follow us! We're friendly:</div>
-            
-              <div className="sm">
-                    <a href="https://github.com/AmandaMitchell707">
-                  <i className="fab fa-github" />
-                  Amanda Mitchell
-                </a>
-            
-                    <a href="https://github.com/cyippiy">
-                  <i className="fab fa-github" />
-                  Chris Yip
-                </a>
+  render() {
+    return <div className="bottom-part">
+        
+      <div className="text">Follow us! We're friendly:</div>
+    
+      <div className="sm">
+        <a href="https://github.com/AmandaMitchell707">
+          <i className="fab fa-github" />
+          Amanda Mitchell
+        </a>
+    
+        <a href="https://github.com/cyippiy">
+          <i className="fab fa-github" />
+          Chris Yip
+        </a>
 
-                <a href="https://github.com/parisDreyer">
-                  <i className="fab fa-github" />
-                  Luke Dreyer
-                </a>
+        <a href="https://github.com/parisDreyer">
+          <i className="fab fa-github" />
+          Luke Dreyer
+        </a>
 
-                <a href="https://github.com/nathashas1">
-                  <i className="fab fa-github" />
-                  Nathasha Surendran
-                </a>
-              </div>
-            
-            
-
-          </div>;
-            
-          
-    }
+        <a href="https://github.com/nathashas1">
+          <i className="fab fa-github" />
+          Nathasha Surendran
+        </a>
+        <div id="edamam-badge" data-color="white"></div>
+      </div>
+    </div>;
+  }
 }
 
 export default Footer;
